01d685c40fc3cdf6d65015f13bcde4cc2b99ed83
sehoon0302 <saehun66@gmail.com>
2023-10-29 14:40:27 +0900

    메인화면에서 전체화면 사진으로 이돌

--- app/src/main/java/kr/ac/wku/librarypractice_20231029/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/librarypractice_20231029/MainActivity.kt
@@ -1,11 +1,24 @@
 package kr.ac.wku.librarypractice_20231029
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.ac.wku.librarypractice_20231029.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.imgProfile.setOnClickListener {
+            //이미지뷰도 클릭 이벤트 지원
+
+            //ViewPhotoActivity로 이동
+            var myIntent = Intent(this, ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }

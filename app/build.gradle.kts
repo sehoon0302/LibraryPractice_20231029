6d6570f7fe1e7102d0086a0ad55b951d10f5ae48
sehoon0302 <saehun66@gmail.com>
2023-10-29 15:02:04 +0900

    PhotoView 라이브러리를 활용하여 줌인/줌아웃 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,6 @@ android {
 
 dependencies {
 
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -49,4 +48,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.github.chrisbanes:PhotoView:2.3.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
